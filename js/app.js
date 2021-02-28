da751457e0b05f8c65fb1cef73a51505b3691b35
Ismail Boularbah <qiranorane55@gmail.com>
2021-02-28 19:19:58 +0100

    initial commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+//const card = document.querySelectorAll(".card");
+//
+//card.forEach((item) =>
+//  item.addEventListener("click", () => {
+//    card.forEach((element) => (element.className = "card"));
+//    if (item.className == "card active") item.className = "card";
+//    else item.className = "card active";
+//  })
+//);
